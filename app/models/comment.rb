7252487b2307f97501dee814f6a2747583c57ce0
abeidahmed <abeidahmed92@gmail.com>
2021-01-28 14:28:36 +0530

    Add comment model validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
+  has_many :comments, as: :commentable, dependent: :destroy
   belongs_to :commentable, polymorphic: true
   belongs_to :user
+
+  validates :content, presence: true, length: { maximum: 500 }
 end
