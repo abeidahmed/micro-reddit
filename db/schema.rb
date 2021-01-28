9a3612b0c57aa5f31eca3995bc5e1c3fb117bf31
abeidahmed <abeidahmed92@gmail.com>
2021-01-28 14:17:38 +0530

    Initialize comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_28_084003) do
+ActiveRecord::Schema.define(version: 2021_01_28_084649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content", default: "", null: false
+    t.string "commentable_type", null: false
+    t.bigint "commentable_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title", default: "", null: false
@@ -33,5 +44,6 @@ ActiveRecord::Schema.define(version: 2021_01_28_084003) do
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
