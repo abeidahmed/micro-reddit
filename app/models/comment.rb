b3a20862c479d7e882525ec8c6e3ac058e63f9fb
abeidahmed <abeidahmed92@gmail.com>
2021-01-28 14:34:01 +0530

    Setup vote, user, post, comment association

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
   has_many :comments, as: :commentable, dependent: :destroy
+  has_many :votes, as: :votable, dependent: :destroy
   belongs_to :commentable, polymorphic: true
   belongs_to :user
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
 
   before_validation :normalize_email_address
 
