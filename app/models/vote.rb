fb237a6d18bbd6c2e64687eb0dfa0595be9a325f
abeidahmed <abeidahmed92@gmail.com>
2021-01-28 14:52:34 +0530

    Remove vote model and instead handle on table col

--- app/models/vote.rb
+++
@@ -1,4 +0,0 @@
-class Vote < ApplicationRecord
-  belongs_to :votable, polymorphic: true
-  belongs_to :user
-end
--- db/migrate/20210128084003_create_posts.rb
+++ db/migrate/20210128084003_create_posts.rb
@@ -3,9 +3,12 @@ class CreatePosts < ActiveRecord::Migration[6.1]
     create_table :posts do |t|
       t.string :title, null: false, default: ''
       t.text :content, default: ''
+      t.integer :vote, null: false, default: 0
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
+
+    add_index :posts, :vote
   end
 end
