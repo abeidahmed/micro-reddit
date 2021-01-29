8f776b32fae415982da725d3515da9d378fce854
abeidahmed <abeidahmed92@gmail.com>
2021-01-29 13:37:46 +0530

    Fix rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i # rubocop:disable Style/MutableConstant
 
   validates :full_name, presence: true, length: { maximum: 255 }
   validates(
